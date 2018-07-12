d0b2b73595679bec6b55935b8c7f2d383ac54c6f
William Copeland <willcopeland34@gmail.com>
2018-07-11 22:09:22 -0700

    Service Call pricing table added

--- service.php
+++ service.php
@@ -32,6 +32,191 @@
   <!-- Some information about fleet services -->
 
   <!-- Information about regualar worry-free contract services -->
+
+  <div class="service-call-table">
+    <h2>Service Call Pricing</h2>
+    
+    <table>
+      <tr>
+        <th>CITY</th>
+        <th>SERVICE</th>
+        <th>CITY</th>
+        <th>SERVICE</th>
+      </tr>
+      <tr>
+        <td>ALAMEDA</td>
+        <td>$75</td>
+        <td>NAPA</td>
+        <td>$95</td>
+      </tr>
+      <tr>
+        <td>ALAMO</td>
+        <td>$55</td>
+        <td>NEWARK</td>
+        <td>$75</td>
+      </tr>
+      <tr>
+        <td>ANTIOCH</td>
+        <td>$55</td>
+        <td>NOVATO</td>
+        <td>$95</td>
+      </tr>
+      <tr>
+        <td>BAYPOINT</td>
+        <td>$55</td>
+        <td>OAKLAND</td>
+        <td>$75</td>
+      </tr>
+      <tr>
+        <td>BENICIA</td>
+        <td>$65</td>
+        <td>NOVATO</td>
+        <td>$75</td>
+      </tr>
+      <tr>
+        <td>BERKELEY</td>
+        <td>$75</td>
+        <td>PACHECO</td>
+        <td>$55</td>
+      </tr>
+      <tr>
+        <td>BETHL ISLAND</td>
+        <td>$65</td>
+        <td>PALO ALTO</td>
+        <td>$125</td>
+      </tr>
+      <tr>
+        <td>BRENTWOOD</td>
+        <td>$55</td>
+        <td>PITTSBURG</td>
+        <td>$55</td>
+      </tr>
+      <tr>
+        <td>BYRON</td>
+        <td>$55</td>
+        <td>PLESANT HILL</td>
+        <td>$55</td>
+      </tr>
+      <tr>
+        <td>CASTRO VALLEY</td>
+        <td>$75</td>
+        <td>PLEASNTON</td>
+        <td>$75</td>
+      </tr>
+      <tr>
+        <td>CLAYTON</td>
+        <td>$55</td>
+        <td>REDWOOD CITY</td>
+        <td>$125</td>
+      </tr>
+      <tr>
+        <td>CONCORD</td>
+        <td>$55</td>
+        <td>RICHMOND</td>
+        <td>$75</td>
+      </tr>
+      <tr>
+        <td>DANVILLE</td>
+        <td>$65</td>
+        <td>RIO VISTA</td>
+        <td>$75</td>
+      </tr>
+      <tr>
+        <td>DIABLO</td>
+        <td>$65</td>
+        <td>ROSEVILLE</td>
+        <td>$225</td>
+      </tr>
+      <tr>
+        <td>DISCOVERY BAY</td>
+        <td>$55</td>
+        <td>SACROMENTO</td>
+        <td>$225</td>
+      </tr>
+      <tr>
+        <td>DUBLIN</td>
+        <td>$75</td>
+        <td>SAN FRANCISCO</td>
+        <td>$125</td>
+      </tr>
+      <tr>
+        <td>EL SOBRANTE</td>
+        <td>$65</td>
+        <td>SAN JOSE</td>
+        <td>$125</td>
+      </tr>
+      <tr>
+        <td>FAIRFIELD</td>
+        <td>$65</td>
+        <td>SAN MATEO</td>
+        <td>$125</td>
+      </tr>
+      <tr>
+        <td>FREMONT</td>
+        <td>$75</td>
+        <td>SAN RAFAEL</td>
+        <td>$95</td>
+      </tr>
+      <tr>
+        <td>HALF MOON BAY</td>
+        <td>$225</td>
+        <td>SAN RAMON</td>
+        <td>$75</td>
+      </tr>
+      <tr>
+        <td>HAYWARD</td>
+        <td>$75</td>
+        <td>SANTA CRUZ</td>
+        <td>$295</td>
+      </tr>
+      <tr>
+        <td>HERCULES</td>
+        <td>$65</td>
+        <td>SANTA ROSA</td>
+        <td>$125</td>
+      </tr>
+      <tr>
+        <td>ILSETON</td>
+        <td>$75</td>
+        <td>SARATOGA</td>
+        <td>$150</td>
+      </tr>
+      <tr>
+        <td>LIVERMORE</td>
+        <td>$75</td>
+        <td>SONOMA</td>
+        <td>$125</td>
+      </tr>
+      <tr>
+        <td>MARTINEZ</td>
+        <td>$55</td>
+        <td>SUNNYVALE</td>
+        <td>$125</td>
+      </tr>
+      <tr>
+        <td>MILL VALLEY</td>
+        <td>$55</td>
+        <td>TRACY</td>
+        <td>$95</td>
+      </tr>
+      <tr>
+        <td>MILPITAS</td>
+        <td>$75</td>
+        <td>VACAVILLE</td>
+        <td>$65</td>
+      </tr>
+      <tr>
+        <td>MORAGA</td>
+        <td>$65</td>
+        <td>VALLEJO</td>
+        <td>$75</td>
+      </tr>
+      <tr>
+        <td>WALNUT CREEK</td>
+        <td>$55</td>
+      </tr>
+    </table>
+  </div>
 </div>
 
 
